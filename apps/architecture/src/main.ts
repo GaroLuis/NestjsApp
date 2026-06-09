793fbe92bb56cd291ff25991ee59a9fdec146bab
GaroLuis <garoluis94@gmail.com>
2026-06-09 17:15:23 +0200

    feat: add .env files

--- apps/architecture/src/main.ts
+++ apps/architecture/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+config({ path: '.env' });
+const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env.dev';
+config({ path: envFile });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
